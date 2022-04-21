59d265237fcb4e512df58760a34f710909d94d94
Marcelo Dias Schneider <marcelo.d.schneider@gmail.com>
2022-04-21 12:33:12 +0200

    condition to punchline

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -1,19 +1,28 @@
+import { useState } from "react";
 import Comment from "./Comment";
 
 function Joke({ setup, punchline, upvotes, donwvotes, comments }) {
-  comments = comments ? comments.map((comment, key)=> <Comment key={key} comment={comment}/>) : '' ;
+  comments = comments
+    ? comments.map((comment, key) => <Comment key={key} comment={comment} />)
+    : "";
+  const [isShown, setIsShown] = useState(false);
+  function toggleIsShown() {
+    setIsShown((isShown) => !isShown);
+  }
+
   return (
     <div>
       <h3 style={{ display: setup ? "block" : "none" }}>Setup: {setup}</h3>
-      {punchline && <p>Punchline: {punchline}</p>}
-      { upvotes && donwvotes &&
+      {((punchline && isShown) || !setup) && <p>Punchline: {punchline}</p>}
+      {setup && <button onClick={toggleIsShown}>Show punchline</button>}
+      {upvotes && donwvotes && (
         <p>
           <span className="mute">upvotes: </span>
           {upvotes}
           <span className="mute"> | downvotes: </span>
           {donwvotes}
         </p>
-      }
+      )}
       {comments && comments}
       <hr />
     </div>
